04594568e7d760d34d07184374bb02081cd74262
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Pro.local>
2024-06-10 14:20:16 +0800

    Revert "HRIS-361 [Task] Update the actions yml file"
    This reverts commit ceff07b742a8a31f35ebfb7010a0ede84c911204.

--- api_v2/src/app.controller.ts
+++ api_v2/src/app.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthTokenService } from './auth-token/auth-token.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly authTokenService: AuthTokenService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello() {
-    console.log(await this.authTokenService.getAuthToken());
+  getHello(): string {
     return this.appService.getHello();
   }
 }
--- api_v2/src/app.module.ts
+++ api_v2/src/app.module.ts
@@ -8,29 +8,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { AppResolver } from './app.resolver';
-import { ScheduleModule } from '@nestjs/schedule';
-import { TaskModule } from './task/task.module';
-import { SlackModule } from './slack/slack.module';
-import { HrisApiModule } from './hris-api/hris-api.module';
-import { AuthTokenModule } from './auth-token/auth-token.module';
-import slackConfig from 'config/slack.config';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [jwtConfig, slackConfig],
+      load: [jwtConfig],
     }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      global: true,
       useFactory: (configService: ConfigService) => ({
+        global: true,
         secret: configService.get('jwtSecret'),
-        signOptions: { 
-          expiresIn: configService.get('jwtSigningExpiration'), 
-          algorithm: configService.get('jwtSigningAlgorithm') 
-        },
+        signOptions: { expiresIn: '1h' },
       }),
       inject: [ConfigService],
     }),
@@ -40,19 +30,6 @@ import { CacheModule } from '@nestjs/cache-manager';
         autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
       }),
     }),
-    ScheduleModule.forRoot(),
-    CacheModule.registerAsync({
-      imports: [ConfigModule],
-      isGlobal: true,
-      useFactory: (configService: ConfigService) => ({
-        ttl: configService.get('cacheTTL')
-      }),
-      inject: [ConfigService],
-    }),
-    TaskModule,
-    SlackModule,
-    HrisApiModule,
-    AuthTokenModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
