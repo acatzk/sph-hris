ceff07b742a8a31f35ebfb7010a0ede84c911204
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Pro.local>
2024-06-10 14:09:39 +0800

    HRIS-361 [Task] Update the actions yml file

--- api_v2/config/jwt.config.ts
+++ api_v2/config/jwt.config.ts
@@ -1,3 +1,5 @@
 export default () => ({
-    jwtSecret: process.env.JWT_SECRET_KEY
+    jwtSecret: process.env.JWT_SECRET_KEY,
+    jwtSigningExpiration: process.env.JWT_SIGNING_EXPIRATION || '7d',
+    jwtSigningAlgorithm: process.env.JWT_SIGNING_ALGORITHM || 'HS256',
 })
--- api_v2/src/app.controller.ts
+++ api_v2/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthTokenService } from './auth-token/auth-token.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authTokenService: AuthTokenService) {}
 
   @Get()
-  getHello(): string {
+  async getHello() {
+    console.log(await this.authTokenService.getAuthToken());
     return this.appService.getHello();
   }
 }
