0b538bafd41c91ac59a04303f142635f19228a03
Andre Lennard Uy <andrelennard.uy@sun-asterisk.com>
2024-07-01 16:13:32 +0800

    fix issues and comments

--- api_v2/src/work-interruption/work-interruption.resolver.ts
+++ api_v2/src/work-interruption/work-interruption.resolver.ts
@@ -4,8 +4,19 @@ import { CreateInterruptionRequestInput } from '@/graphql/graphql';
 
 @Resolver('WorkInterruption')
 export class WorkInterruptionResolver {
-  constructor(private readonly workInterruptionService: WorkInterruptionService) {}
+  constructor(
+    private readonly workInterruptionService: WorkInterruptionService,
+  ) {}
 
+  /**
+   *Handle Creating a new work interruption
+   *
+   *
+   * @param {CreateInterruptionRequestInput} interruption this input require TimeEntryId and
+   * WorkInterruptionTypeId
+   *
+   * @returns the Id of the new work interruption
+   */
   @Mutation('createWorkInterruption')
   create(@Args('interruption') interruption: CreateInterruptionRequestInput) {
     return this.workInterruptionService.create(interruption);
--- api_v2/src/work-interruption/work-interruption.service.spec.ts
+++ api_v2/src/work-interruption/work-interruption.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkInterruptionService } from './work-interruption.service';
+import { PrismaService } from '@/prisma/prisma.service';
 
 describe('WorkInterruptionService', () => {
   let service: WorkInterruptionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkInterruptionService],
+      providers: [WorkInterruptionService, PrismaService],
     }).compile();
 
     service = module.get<WorkInterruptionService>(WorkInterruptionService);
