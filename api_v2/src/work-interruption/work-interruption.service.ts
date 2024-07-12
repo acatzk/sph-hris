335912cf272082eed9550405f221a4927365857a
Fontillas <johnrussel.fontillas@sun-asterisk.com>
2024-07-12 13:33:03 +0800

    hotfix/HRIS-381 Migrate My Daily Time Record > View Work Interruptions  > Edit Work Interruption

--- api_v2/src/work-interruption/work-interruption.service.ts
+++ api_v2/src/work-interruption/work-interruption.service.ts
@@ -179,6 +179,7 @@ export class WorkInterruptionService {
           workInterruptionTypeId: workInterruptionTypeId
             ? workInterruptionTypeId
             : undefined,
+          updatedAt: getCurrentDate().toString(),
         },
       });
 
