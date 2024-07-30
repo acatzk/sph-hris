9dd30e8b548eb4b442c217944f7f6e9aaec64743
sunasterisk.jejercito <jamesandrew.ejercito@sun-asterisk.com>
2024-07-30 12:42:14 +0800

    created unit tests

--- api_v2/src/time-record/time-record.resolver.spec.ts
+++ api_v2/src/time-record/time-record.resolver.spec.ts
@@ -4,22 +4,39 @@ import { TimeRecordService } from './time-record.service';
 
 describe('TimeRecordResolver', () => {
   let resolver: TimeRecordResolver;
-  const mockTimeRecordService = {
-    getTimeEntriesById  : jest.fn()
+  let mockTimeRecordService = {
+    getUserById: jest.fn().mockResolvedValue({id: 1, name: 'Abdul Jalil Palala'}),
+    getTimeEntriesById: jest.fn().mockResolvedValue({startTime: "09:30:00"}),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TimeRecordResolver, TimeRecordService],
-    })
-    .overrideProvider(TimeRecordService)
-    .useValue(mockTimeRecordService)
-    .compile();
+      providers: [TimeRecordResolver, {
+        provide: TimeRecordService,
+        useValue: mockTimeRecordService
+      }],
+    }).compile();
 
     resolver = module.get<TimeRecordResolver>(TimeRecordResolver);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('userById', () => {
+    it('should call the service to get user', async () => {
+      const result = await resolver.userById(1);
+
+      expect(result).toEqual({id: 1, name: 'Abdul Jalil Palala'});
+    });
+  });
+
+  describe('timeEntriesByEmployeeId', () => {
+    it('should call the service to get time entries', async () => {
+      const result = await resolver.timeEntriesByEmployeeId(1);
+
+      expect(result).toEqual({startTime: "09:30:00"});
+    });
   });
 });
